93260dbc0f6754aca8d3848343ead79d9e89d68c
Jagmeet Singh <jagmeet_si@HCL.COM>
2019-04-03 12:08:55 +0530

    removed loger

--- src/main/java/com/currency/converter/convert/Interceptors.java
+++ src/main/java/com/currency/converter/convert/Interceptors.java
@@ -3,8 +3,7 @@ package com.currency.converter.convert;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class Interceptors implements HandlerInterceptor {
 	
-	Logger logger = LoggerFactory.getLogger(Interceptors.class);
+
 	public Interceptors() {
 		// TODO Auto-generated constructor stub
 	}
@@ -22,8 +21,7 @@ public class Interceptors implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
-		logger.info(">>>>controller is going to do its work>>>>");
+	
 		return true;
 	}
 
@@ -31,7 +29,7 @@ public class Interceptors implements HandlerInterceptor {
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		// TODO Auto-generated method stub
-		logger.info(">>>>controller done its work>>>>");
+
 	}
 
 
